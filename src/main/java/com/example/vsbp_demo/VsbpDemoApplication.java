8b1f700db5e462cf64b2018aa28a00b843612f4f
okamura-keisuke <b2220530@photon.chitose.ac.jp>
2024-12-27 18:46:13 +0900

    first commit

---
+++ src/main/java/com/example/vsbp_demo/VsbpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vsbp_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsbpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsbpDemoApplication.class, args);
+	}
+
+}
